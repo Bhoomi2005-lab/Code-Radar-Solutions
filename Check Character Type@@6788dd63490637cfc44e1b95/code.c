fee9a07868b8aef2bc48c087a9ed590a74835d45
Bhoomi2005-lab <dhingrabhoomi108@gmail.com>
2025-01-30 17:26:58 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    char c;
-    scanf("%c",&c);
-    if((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u')||(c=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U'))
+    char a;
+    scanf("%c",&a);
+    if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u')||(a=='A')||(a=='E')||(a=='I')||(a=='O')||(a=='U'))
     {
         printf("Vowel");
     }
-    else if((c>='0')&&(c<='9'))
+    else if((a>='0')&&(a<='9'))
     {
         printf("Digit");
     }
-    else if((c!='a')&&(c!='e')&&(c!='i')&&(c!='o')&&(c!='u')&&(c!='A')&&(c!='E')&&(c!='I')&&(c!='O')&&(c!='U'))
+    else if((a!='a')&&(a!='e')&&(a!='i')&&(a!='o')&&(a!='u')&&(a!='A')&&(a!='E')&&(a!='I')&&(a!='O')&&(a!='U'))
     {
         printf("Consonant");
     }
